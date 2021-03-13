9666adc75843e542cdaaecd163591cac4d4154f9
kuonuchi <kuokillua1228@gmail.com>
2021-03-14 05:03:05 +0800

    week-3

---
+++ week-3/Assignment/index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const { render } = require('pug');
+const app = express();
+const port = 3000;
+const router = express.Router();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static('public'));
+
+
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+
+
+app.get('/data', (req, res) => {
+    const { Number } = req.query
+
+    if(Number == null){
+        res.send('Lack of Parameter');
+
+    }else if(isNaN(Number)){
+        res.send('Wrong Parameter');
+        
+    }else{
+        
+        let add = ''
+        let aws = 0;
+        for(let i=1; i<= Number; i++){
+            add += ""+i+"";
+            aws += i;
+            if(Number != i){
+                add += ' + ';
+            }
+            if(Number % 45 == 0){
+                add += '\n';
+            }
+        }
+        res.send(add);
+        console.log(Number);
+    }
+
+    
+});
+
+
+app.get('/myName', (req, res) => {
+    const cookiename = req.cookies.username
+    
+    if(cookiename){
+        res.send('yor name is: '+cookiename);
+    }else{
+        res.redirect('/trackName');
+    }
+    
+    
+});
+
+app.get('/trackName', (req, res) => {
+    const cookiename = req.cookies.username
+    if(cookiename){
+        res.redirect('/myName');
+    }
+    res.render('index');
+
+});
+
+
+app.post('/trackName', (req, res) => {
+    
+    res.cookie('username', req.body.username);
+    res.redirect('/myName');
+    
+    
+});
+
+
+app.listen(3000, () => {
+  console.log("ready");
+});
+
---
+++ week-3/Assignment/public/index.js
@@ -0,0 +1,5 @@
+let btm = document.querySelector(".box2");
+
+btm.addEventListener("click", ()=>{
+    window.location.href = 'https://www.youtube.com/watch?v=072tU1tamd0'
+});
