21ac2f3e3eaf872ff2d8793aaa171d60ffb7a2d0
kuonuchi <kuokillua1228@gmail.com>
2021-03-14 18:44:39 +0800

    finised week-3 Assignment-5

--- week-3/Assignment 5/index.js
+++ week-3/Assignment 5/index.js
@@ -1,17 +1,30 @@
 function twoSum(nums, target) { // your code here 
+    var answer=[];
+
+    //無情爆搜，硬找answer
     for(let i=0; i<nums.length; i++){
-        
+        for(let u=0; u<nums.length; u++){
+            if(i != u){
+                if(nums[i]+nums[u] == target){
+                    answer[0] = i;
+                    answer[1] = u;
+                    return answer;
+                }
+            }
+
+            
+        }
     }
 }
 
-/*
 
-For example:
 
-twoSum([2, 7, 11, 15], 9); Should returns:
+// For example:
 
-[0, 1] Because:
+console.log(twoSum([2, 11, 7, 15], 9));
 
-nums[0]+nums[1] is 9
+// Should returns:
+// [0, 1] Because:
 
-*/
+// nums[0]+nums[1] is 9
+
